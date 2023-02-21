2dc6697011fbd8a33d8626d4e6dc9466e9af9bc4
zj5fzq <srinivas.r.bhat@email.com>
2023-02-21 16:34:07 +0530

    Added Chat - Non Fuctional

--- src/GroupChat.js
+++ src/GroupChat.js
@@ -1,5 +1,28 @@
 function GroupChat() {
-    return <div>This is Group Chat Functional Component</div>
-}
+    return (
+        <div>
+            <span className="tab"><a className="tool_box" style={{ backgroundColor: "white" }} href="groupchat">Group Chat</a></span>
+            <span className="tab"><a className="tool_box" href="manageusers">Manage Users</a></span>
+            <span className="tab"><a className="tool_box" href="managedocuments">Manage Documents</a></span>
+            <span className="tab"><a className="tool_box" href="logout">Logout</a></span>
+            <div className="chat_bar">
+                <div className="exit_box">X</div>
+                <p className="align_centre">Group Chat</p>
+            </div>
 
-export default GroupChat;
+            <div className="chats">
+                [2013-27-01 01:00:16] Text User: Lorem ipsum dolor sit amet, <br />
+                [2013-27-01 01:05:22] Text User: consectetur adipisicing elit. <br />
+                [2013-27-01 01:11:14] Anne Hunter: In aliquid temporibus ea? Laudantium voluptate. <br />
+                [2013-27-01 02:11:35] Jack Washk: quo officia laboriosam ab voluptatibus asperiores <br />
+            </div>
+
+            <div className="chat_window">
+                <div className="chat_name"><b>Anne Hunter</b></div>
+                <input name="fileLabel" className="chat_text_box" type="text" placeholder="" />
+                <button className="send">Send</button>
+                <button className="refresh">Refresh</button>
+            </div>
+        </div>
+    )
+} export default GroupChat;
